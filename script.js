263e4b0a19c6da9aea8a64c8f4c863c4b28df4d9
Waseem Brare <waseembrare@gmail.com>
2021-02-21 09:44:24 +0000

    css styling, favicon and footer
    Changed filenames in soundclips object within JS file to be all lowercase. Added two favicons into assets directory so damien can choose his preference. Added a footer in the  html file. Changed body resets in the css file as well as styling the buttons into a flex box with wrapping. Body font changed to digital 7 mono to match the theme of the images.

--- script.js
+++ script.js
@@ -1,186 +1,178 @@
-// const sounds = [
-//     {
-//         title: "80 million people",
-//         'src': "assets/sounds/80-million-people.mp3"
-//     },
-//     {
-//         title: "Are you sure",
-//         'src': "assets/sounds/sounds/Are-you-sure.mp3"
-//     },
-//     {
-//         title: "Bastard",
-//         'src': "assets/sounds/Bastard.mp3"
-//     },
-//     {
-//         title: "Come down here",
-//         'src': "assets/sounds/Come-down-here.mp3"
-//     },
-//     {
-//         title: "Does that make you feel stressed",
-//         'src': "assets/sounds/Does-that-make-you-feel-stressed.mp3"
-//     },
-//     {
-//         title: "Easy to remember",
-//         'src': "assets/sounds/Easy-to-remember.mp3"
-//     },
-//     {
-//         title: "Fiddle about with it",
-//         'src': "assets/sounds/Fiddle-About-With-It.mp3"
-//     },
-//     {
-//         title: "Fire",
-//         'src': "assets/sounds/Fire.mp3"
-//     },
-//     {
-//         title: "Fix my pants",
-//         'src': "assets/sounds/Fix-My-Pants.mp3"
-//     },
-//     {
-//         title: "Flip off",
-//         'src': "assets/sounds/Flip-off.mp3"
-//     },
-//     {
-//         title: "Forcing unexpected reboot",
-//         'src': "assets/sounds/Forcing-unexpected-reboot.mp3"
-//     },
-//     {
-//         title: "Have you tried sticking it up",
-//         'src': "assets/sounds/Have-you-tried-sticking-it-up.mp3"
-//     },
-//     {
-//         title: "Have you tried",
-//         'src': "assets/sounds/have-you-tried.mp3"
-//     },
-//     {
-//         title: "Hello IT 2",
-//         'src': "assets/sounds/Hello-IT-2.mp3"
-//     },
-//     {
-//         title: "Hello IT",
-//         'src': "assets/sounds/Hello-IT.mp3"
-//     },
-//     {
-//         title: "I am declaring war",
-//         'src': "assets/sounds/I-am-declaring-war.mp3"
-//     },
-//     {
-//         title: "I lie",
-//         'src': "assets/sounds/I-lie.mp3"
-//     },
-//     {
-//         title: "I told her",
-//         'src': "assets/sounds/I-told-her.mp3"
-//     },
-//     {
-//         title: "I'm disabled",
-//         'src': "assets/sounds/Im-Disabled.mp3"
-//     },
-//     {
-//         title: "I'm sorry",
-//         'src': "assets/sounds/Im-sorry.mp3"
-//     },
-//     {
-//         title: "It can't be done",
-//         'src': "assets/sounds/It-cant-be-done.mp3"
-//     },
-//     {
-//         title: "Loosing blood",
-//         'src': "assets/sounds/Loosing-blood.mp3"
-//     },
-//     {
-//         title: "Moss ringtone",
-//         'src': "assets/sounds/Moss-ringtone.mp3"
-//     },
-//     {
-//         title: "Need to turn it on",
-//         'src': "assets/sounds/Need-to-turn-it-on.mp3"
-//     },
-//     {
-//         title: "New emergency number",
-//         'src': "assets/sounds/New-Emergency-Number.mp3"
-//     },
-//     {
-//         title: "Nice screensaver",
-//         'src': "assets/sounds/Nice-screensaver.mp3"
-//     },
-//     {
-//         title: "",
-//         'src': "assets/sounds/"
-//     },
-//     {
-//         title: "",
-//         'src': "assets/sounds/"
-//     },
-//     {
-//         title: "",
-//         'src': "assets/sounds/"
-//     },
-//     {
-//         title: "",
-//         'src': "assets/sounds/"
-//     },
-//     {
-//         title: "",
-//         'src': "assets/sounds/"
-//     },
-//     {
-//         title: "",
-//         'src': "assets/sounds/"
-//     },
-//     {
-//         title: "",
-//         'src': "assets/sounds/"
-//     },
-//     {
-//         title: "",
-//         'src': "assets/sounds/"
-//     },
-//     {
-//         title: "",
-//         'src': "assets/sounds/"
-//     }
-// ]
-
 const soundClips = [
     {
         title: "80 million people",
         src: "assets/sounds/80-million-people.mp3"
     },
     {
-        title: "Are you sure",
-        src: "assets/sounds/Are-you-sure.mp3"
+        title: "are you sure",
+        src: "assets/sounds/are-you-sure.mp3"
+    },
+    {
+        title: "bastard",
+        src: "assets/sounds/bastard.mp3"
+    },
+    {
+        title: "come down here",
+        src: "assets/sounds/come-down-here.mp3"
+    },
+    {
+        title: "does that make you feel stressed",
+        src: "assets/sounds/does-that-make-you-feel-stressed.mp3"
+    },
+    {
+        title: "easy to remember",
+        src: "assets/sounds/easy-to-remember.mp3"
+    },
+    {
+        title: "fiddle about with it",
+        src: "assets/sounds/fiddle-about-with-it.mp3"
+    },
+    {
+        title: "fire",
+        src: "assets/sounds/fire.mp3"
+    },
+    {
+        title: "fix my pants",
+        src: "assets/sounds/fix-my-pants.mp3"
+    },
+    {
+        title: "flip off",
+        src: "assets/sounds/flip-off.mp3"
+    },
+    {
+        title: "forcing unexpected reboot",
+        src: "assets/sounds/forcing-unexpected-reboot.mp3"
+    },
+    {
+        title: "have you tried sticking it up",
+        src: "assets/sounds/have-you-tried-sticking-it-up.mp3"
+    },
+    {
+        title: "have you tried",
+        src: "assets/sounds/have-you-tried.mp3"
+    },
+    {
+        title: "hello it moss",
+        src: "assets/sounds/hello-it-2.mp3"
+    },
+    {
+        title: "hello it",
+        src: "assets/sounds/hello-it.mp3"
+    },
+    {
+        title: "i am declaring war",
+        src: "assets/sounds/i-am-declaring-war.mp3"
+    },
+    {
+        title: "i lie",
+        src: "assets/sounds/i-lie.mp3"
+    },
+    {
+        title: "i told her",
+        src: "assets/sounds/i-told-her.mp3"
+    },
+    {
+        title: "i'm disabled",
+        src: "assets/sounds/im-disabled.mp3"
+    },
+    {
+        title: "i'm sorry",
+        src: "assets/sounds/im-sorry.mp3"
+    },
+    {
+        title: "it can't be done",
+        src: "assets/sounds/it-cant-be-done.mp3"
+    },
+    {
+        title: "loosing blood",
+        src: "assets/sounds/loosing-blood.mp3"
+    },
+    {
+        title: "moss ringtone",
+        src: "assets/sounds/moss-ringtone.mp3"
+    },
+    {
+        title: "need to turn it on",
+        src: "assets/sounds/need-to-turn-it-on.mp3"
+    },
+    {
+        title: "new emergency number",
+        src: "assets/sounds/new-emergency-number.mp3"
+    },
+    {
+        title: "nice screensaver",
+        src: "assets/sounds/nice-screensaver.mp3"
+    },
+    {
+        title: "not afraid",
+        src: "assets/sounds/pull-down-my-trousers.mp3"
+    },
+    {
+        title: "pull down my trousers",
+        src: "assets/sounds/pull-down-my-trousers.mp3"
+    },
+    {
+        title: "sent an email",
+        src: "assets/sounds/sent-an-email.mp3"
+    },
+    {
+        title: "stop",
+        src: "assets/sounds/stop.mp3"
+    },
+    {
+        title: "the it crowd opening theme",
+        src: "assets/sounds/the-it-crowd-opening-theme.mp3"
+    },
+    {
+        title: "you burk",
+        src: "assets/sounds/you-burk.mp3"
+    },
+    {
+        title: "you there fix my pants",
+        src: "assets/sounds/you-there-fix-my-pants.mp3"
+    },
+    {
+        title: "you're in big trouble",
+        src: "assets/sounds/youre-in-big-trouble.mp3"
     }
-];
+]
 
+// const soundClips = [
+//     {
+//         title: "80 million people",
+//         src: "assets/sounds/80-million-people.mp3",
+//         keystroke: "q"
+//     },
+//     {
+//         title: "are you sure",
+//         src: "assets/sounds/are-you-sure.mp3",
+//         keystroke: "w"
+//     }
+// ];
 
-
-// let outputButtons = document.querySelector('#button-container').innerHTML
-
+// let outputButtons = document.querySelector('#button-container')
 // soundClips.forEach((clip) => {
 //     document.querySelector('#button-container').innerHTML += '<div class="button" data-src="' + clip.src + '">' + clip.title + '</div>';
 // })
 
 soundClips.forEach((clip) => {
+    // outputButtons.innerHTML += '<div id="' + clip.title + '" class="button" data-src="' + clip.src + '">' + clip.title + '</div>';
     document.querySelector('#button-container').innerHTML += '<div id="' + clip.title + '" class="button" data-src="' + clip.src + '">' + clip.title + '</div>';
+
 })
 
 let buttonElements = document.querySelectorAll('.button');
-
 buttonElements.forEach((title)=>{
     title.addEventListener('click', ()=>{
-        // alert(title.getAttribute('data-src'))
         let audioPath = title.getAttribute('data-src')
-
-        console.log(audioPath)
+        // console.log(audioPath)
         playAudio(audioPath)
-        // console.log(playAudio)
-
     })
 });
 
 function playAudio(audioPath) {
     document.querySelector('#audiobar-container').innerHTML =
-        '<audio src="' + audioPath +  '" </audio>';
+        '<audio src="' + audioPath +  '"> </audio>';
     return document.querySelector('audio').play();
 }
 
@@ -193,31 +185,6 @@ function playAudio(audioPath) {
 //
 // }
 
-// type="audio/mpeg
-
-// <audio
-//     controls
-//     src="/media/cc0-audio/t-rex-roar.mp3">
-// </audio>
-
-// <audio id="myAudio">
-//
-//     <source src="horse.ogg" type="audio/ogg">
-//         <source src="horse.mp3" type="audio/mpeg">
-//             Your browser does not support the audio element.
-// </audio>
-//
 
 
-// FOR IN LOOP
-// // const  = { a: 1, b: 2, c: 3 };
-//
-// for (const property in object) {
-//     console.log(`${property}: ${object[property]}`);
-// }
-
-// expected output:
-// "a: 1"
-// "b: 2"
-// "c: 3"
 
